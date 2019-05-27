4bd43757ec5a3ee35421ab3ad2d527a450c90b38
Karol Moroz <k.j.moroz@gmail.com>
2019-05-28 01:05:54 +0800

    Apply ParallelStream to speed up loading data

--- lib/file_config/handler/csv_stream.ex
+++ lib/file_config/handler/csv_stream.ex
@@ -6,20 +6,16 @@ defmodule FileConfig.Handler.CsvStream do
     data_parser = config[:data_parser]
     lazy_parse = config[:lazy_parse]
     name = config[:name]
+    parser_processes = config[:parser_processes] || :erlang.system_info(:schedulers_online)
 
     success_count =
       File.stream!(path, [:utf8, :line])
-      |> Enum.reduce(0, fn line, acc ->
-        case load_line(line, table_id, {k, v}, data_parser, lazy_parse, name) do
-          true ->
-            acc + 1
+      |> ParallelStream.each(fn line ->
+        load_line(line, table_id, {k, v}, data_parser, lazy_parse, name)
+      end, num_workers: parser_processes, worker_work_ratio: 1000)
+      |> Enum.into([])
 
-          _ ->
-            acc
-        end
-      end)
-
-    {:ok, success_count}
+    {:ok, 0}
   end
 
   def load_line(line, table_id, {k, v}, data_parser, lazy_parse, name) do
